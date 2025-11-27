7ece4bef531afbd10164e1ff64dd72a863c9149e
ERSPITSTAPOL <amusedzys@gmail.com>
2025-11-27 11:23:41 +0800

    更新 _worker.js

--- src/_worker.js
+++ src/_worker.js
@@ -38,13 +38,18 @@ default {
             beianurl: env.BEIANURL || utils.beiandizi,
             configs: utils.configs(env.MIHOMO, env.SINGBOX),
         };
-        e.modes = utils.modes(e.sub, "default");
+        e.modes = utils.modes(e.sub, e.userAgent);
 
-        if (e.urls.length === 1 && e.urls[0].includes(',')) {
-            e.urls = e.urls[0].split(',')
-                .map((u) => u.trim());
+        if (e.urls.length === 1) {
+           let raw = e.urls[0];
+           try {
+               raw = decodeURIComponent(raw);
+           } catch (_) {}
+          if (raw.includes(',')) {
+             e.urls = raw.split(',').map(u => u.trim());
+          }
         }
-
+        
         if (e.urls.length === 0 || e.urls[0] === '') {
             return new Response(await getFakePage(e), {
                 status: 200,
